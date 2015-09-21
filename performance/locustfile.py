0c4bbb56012f5646c52e02e10c462278666f4431
Pawel Kmiecik <pawel.kmiecik@allegrogroup.com>
2015-09-21 08:23:02 +0200

    fix

--- performance/locustfile.py
+++ performance/locustfile.py
@@ -5,9 +5,7 @@ class GetGroup(TaskSet):
     @task
     def group(self):
         for i in range(1000000):
-	    with self.client.get("/group?userId=user%i" % i, name="/group?userId=[userId]", catch_response=True) as response:
-		    if response.status_code == 201:
-			response.failure("User " + str(i) + " created with group " + response.content)
+	    self.client.get("/group?userId=user%i" % i, name="/group?userId=[userId]")
 
 class WebsiteUser(HttpLocust):
     task_set = GetGroup
